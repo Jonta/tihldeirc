699a2be62c81b75d2b9035d7a757abea1e2d652f
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-21 17:52:43 +0200

    realname and username for bot, and correct channel

--- bot.js
+++ bot.js
@@ -6,9 +6,11 @@ plugins = require('plugins.js');
 
 plugins.watch('./plugins/');
 
-exports.channel = '#testfrest';
+exports.channel = '#tihlde';
 
 exports.client = new irc.Client('irc.homelien.no', 'hilde', {
+	userName: 'tihihilde',
+	realName: 'tihihilde',
 	channels: [this.channel]
 });
 
