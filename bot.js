5685a309c25b5e92d02c29c4dffbe09ad8a49a0a
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-21 17:45:39 +0200

    Cleaner setup using plugins.js

--- bot.js
+++ bot.js
@@ -1,32 +1,14 @@
 require.paths.unshift('.');
 
 var fs = require('fs'),
-pluginsDir = "./plugins/",
 irc = require("irc"),
-plugins = {};
+plugins = require('plugins.js');
 
-function watchFiles() {
-	fs.readdir(pluginsDir, function(err, files) {
-		files.forEach(function(file) {
-			file = pluginsDir + file;
-			plugins[file] = require(file);
-			fs.watchFile(file, function(current, previous) {
-				if ( + current.mtime !== + previous.mtime) {
-					plugins[file].destruct && plugins[file].destruct();
-					delete require.cache[require.resolve(file)];
-					plugins[file] = require(file);
-				}
-			});
-		});
-	});
-};
-
-fs.watchFile(pluginsDir, watchFiles);
-watchFiles();
+plugins.watch('./plugins/');
 
 exports.channel = '#testfrest';
 
-exports.client = new irc.Client('irc.homelien.no', 'nodebot', {
+exports.client = new irc.Client('irc.homelien.no', 'hilde', {
 	channels: [this.channel]
 });
 
