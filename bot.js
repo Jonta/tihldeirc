58b058ac4a4bf87ba0e86fa2bfa74432bd32f94a
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-21 17:37:30 +0200

    Very basic setup, trying to keep the code clean

--- bot.js
+++ bot.js
@@ -1,12 +1,32 @@
-var jerk = require('jerk'),
-options = {
-	server: 'irc.homelien.no',
-	nick: 'hilde',
-	channels: ['#tihlde']
+require.paths.unshift('.');
+
+var fs = require('fs'),
+pluginsDir = "./plugins/",
+irc = require("irc"),
+plugins = {};
+
+function watchFiles() {
+	fs.readdir(pluginsDir, function(err, files) {
+		files.forEach(function(file) {
+			file = pluginsDir + file;
+			plugins[file] = require(file);
+			fs.watchFile(file, function(current, previous) {
+				if ( + current.mtime !== + previous.mtime) {
+					plugins[file].destruct && plugins[file].destruct();
+					delete require.cache[require.resolve(file)];
+					plugins[file] = require(file);
+				}
+			});
+		});
+	});
 };
 
-jerk(function(j) {
-	j.watch_for('lett', function(message) {
-		message.say('Like a boss');
-	});
-}).connect(options);
+fs.watchFile(pluginsDir, watchFiles);
+watchFiles();
+
+exports.channel = '#testfrest';
+
+exports.client = new irc.Client('irc.homelien.no', 'nodebot', {
+	channels: [this.channel]
+});
+
---
+++ plugins/twitter.js
@@ -0,0 +1,33 @@
+var bot = require('bot.js'),
+twitter = require('twitter'),
+fs = require('fs'),
+twit;
+
+function onMessage(from, to, message) {
+	if (message.match(/^!twitter /)) {
+		twit.updateStatus(from + ': ' + message.replace(/^!twitter /, ''), function(data) {});
+	}
+}
+
+bot.client.addListener('message', onMessage);
+
+fs.readFile('twitter-keys', function(error, data) {
+	if (!error) {
+
+		twit = new twitter(JSON.parse(data));
+
+		twit.stream('statuses/filter', {
+			track: 'tihlde'
+		},
+		function(stream) {
+			stream.on('data', function(data) {
+				bot.client.say(bot.channel, '[twitter] ' + data.user.screen_name + ': ' + data.text);
+			});
+		});
+	}
+});
+
+exports.destruct = function() {
+	bot.client.removeListener('message', onMessage);
+};
+
