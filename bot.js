d6473187e11d2047872b38efa0c5fa77b68e5689
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-22 14:31:19 +0200

    Formatting

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 require.paths.unshift('.');
 
 var fs = require('fs'),
-irc = require("irc"),
-plugins = require('plugins.js');
+    irc = require("irc"),
+    plugins = require('plugins.js');
 
 plugins.watch('./plugins/');
 
 exports.channel = '#tihlde';
 
 exports.client = new irc.Client('irc.homelien.no', 'hilde', {
-	userName: 'tihihilde',
-	realName: 'tihihilde',
-	channels: [this.channel]
+    userName: 'tihihilde',
+    realName: 'tihihilde',
+    channels: [this.channel]
 });
 
--- plugins/mordi.js
+++ plugins/mordi.js
@@ -1,32 +1,29 @@
 var bot = require('bot.js'),
-fs = require('fs'),
-http = require('http'),
-mordi = JSON.parse(fs.readFileSync('mordi.txt'));
+    fs = require('fs'),
+    http = require('http'),
+    mordi = JSON.parse(fs.readFileSync('mordi.txt'));
 
 function onMessage(from, to, message) {
-	if (message.match(/mordi/)) {
-		var joke = mordi[Math.floor(Math.random() * mordi.length)];
-		console.log(joke)
+    if (message.match(/mordi/)) {
+        var joke = mordi[Math.floor(Math.random() * mordi.length)];
 
-		var options = {
-			host: 'translate.google.com',
-			port: 80,
-			path: '/translate_a/t?client=a&text=' + escape(joke) + '&sl=en&tl=no'
-		};
-		console.log(options);
-		http.get(options, function(res) {
-			res.on('data', function(data) {
-				console.log('' + data);
-				data = JSON.parse(data);
-				bot.client.say(bot.channel, from + ': hehe ja, men ' + data.sentences[0].trans);
-			});
-		});
-	}
+        var options = {
+            host: 'translate.google.com',
+            port: 80,
+            path: '/translate_a/t?client=a&text=' + escape(joke) + '&sl=en&tl=no'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(data) {
+                data = JSON.parse(data);
+                bot.client.say(bot.channel, from + ': hehe ja, men ' + data.sentences[0].trans);
+            });
+        });
+    }
 }
 
 bot.client.addListener('message', onMessage);
 
 exports.destruct = function() {
-	bot.client.removeListener('message', onMessage);
+    bot.client.removeListener('message', onMessage);
 };
 
