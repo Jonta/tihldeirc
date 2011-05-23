a08ed8e05890400f70c0cd37be1f14fb2ba97c65
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-23 12:07:59 +0200

    Moved translate out, might need to reuse it - didnt find any modules for this simple thing

--- plugins/mordi.js
+++ plugins/mordi.js
@@ -1,24 +1,13 @@
 var bot = require('bot.js'),
+    translate = require('translate.js'),
     fs = require('fs'),
-    http = require('http'),
-    Iconv = require('iconv').Iconv,
-    iconv = new Iconv('ISO-8859-1', 'UTF-8'),
     mordi = JSON.parse(fs.readFileSync('assets/mordi.txt'));
 
 function onMessage(from, to, message) {
     if (message.match(/mordi/)) {
         var joke = mordi[Math.floor(Math.random() * mordi.length)];
-
-        var options = {
-            host: 'translate.google.com',
-            port: 80,
-            path: '/translate_a/t?client=a&text=' + escape(joke) + '&sl=en&tl=no'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(data) {
-                data = JSON.parse(iconv.convert(data));
-                bot.client.say(bot.channel, from + ': hehe ja, men ' + data.sentences[0].trans);
-            });
+        translate.translate('en', 'no', joke, function(text) {
+            bot.client.say(bot.channel, from + ': hehe ja, men ' + text);
         });
     }
 }
