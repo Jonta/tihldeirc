0a3b33fdfc531fbe788a8122a6aa15c757e8d0a3
Eirik Brandtzæg <eirikb@eirikb.no>
2011-06-21 12:32:58 +0200

    reply answert to channel if answer it !== undefined

--- plugins/god.js
+++ plugins/god.js
@@ -1,10 +1,15 @@
-var bot = require('bot.js');
+var bot = require('bot.js'),
+t = require('translate.js'),
+g = require('get.js');
 
 function onMessage(from, to, message) {
     if (message.match(/^meh /)) {
         try {
             message = message.replace(/^meh /, '');
-            eval(message);
+            var e = eval(message);
+            if (typeof e !== 'undefined') {
+                bot.say(e);
+            }
         } catch(err) {
             console.log(err);
         }
