a18782c9f19630b6ce26b58d03367b4eebc7e92e
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-12 22:07:55 +0200

    Get uses option now, better and simpler

--- plugins/get.js
+++ plugins/get.js
@@ -3,7 +3,6 @@ path = require('path');
 
 exports.get = function(option, callback) {
     var data = '';
-    url = url.split('/');
     http.get(option, function(res) {
         res.on('data', function(c) {
             data += c;
