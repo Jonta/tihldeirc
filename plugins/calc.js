cc64a7b6e380928e9a2f97083a54b0d3c82fcaee
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-12 22:08:18 +0200

    Always running calc!

---
+++ plugins/calc.js
@@ -0,0 +1,21 @@
+var bot = require('bot.js'),
+get = require('./get.js'),
+winston = require('winston');
+
+bot.addListener(__filename, 'message', function(from, to, msg) {
+    get.get({
+        host: 'www.google.com',
+        path: '/ig/calculator?q=' + encodeURIComponent(msg)
+    },
+    function(data) {
+        try {
+            data = eval('(' + data + ')');
+            if (!data.error || data.error.length === 0) {
+                bot.client.say(to, from + ': ' + data.lhs + ' = ' + data.rhs);
+            }
+        } catch(e) {
+            winston.error('[calc] error: ' + e.toString());
+        }
+    });
+});
+
