220fc4c21e30bb7f808e718d430bcd701e850d3c
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-12 22:08:34 +0200

    New tests for calc, removed console.log

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ client = (function() {
         },
         removeListener: function() {},
         say: function(to, msg) {
-            winston.info('[test] say ' + to.red + (' ' + msg).yellow);
+            winston.info('[test] > ' + to.red + (' ' + msg).yellow);
         }
     };
 } ());
@@ -16,17 +16,18 @@ client = (function() {
 bot.start(client);
 
 function s(msg) {
-    winston.info('[test] s ' + msg.cyan);
+    winston.info('[test] < ' + msg.cyan);
     listeners.forEach(function(l) {
         l('eirikb', '#test', msg);
     });
 }
 
 setTimeout(function() {
-    console.log('ooh, hai');
     s('!e 1+1');
     s('google.com');
     s('s/google/microsoft');
+    s('1 +  1');
+    s('4 eur to nok');
     s('gogle er best');
     s('google*');
     setTimeout(process.exit, 500);
