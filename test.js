98a14e528490eb0e39ffc8e1ca7d26d583c20224
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-12 10:49:15 +0200

    Decided to add this test-file since others might find it helpful

---
+++ test.js
@@ -0,0 +1,32 @@
+var bot = require('./bot.js'),
+winston = require('winston'),
+listeners = [],
+client = (function() {
+    return {
+        addListener: function(type, c) {
+            listeners.push(c);
+        },
+        removeListener: function() {},
+        say: function(to, msg) {
+            winston.info('[test] say ' + to.red + (' ' + msg).yellow);
+        }
+    };
+} ());
+
+bot.start(client);
+
+function s(msg) {
+    winston.info('[test] s ' + msg);
+    listeners.forEach(function(l) {
+        l('eirikb', '#test', msg);
+    });
+}
+
+setTimeout(function() {
+    console.log('ooh, hai');
+    s('!e 1+1');
+    s('google.com');
+    s('s/google/microsoft');
+},
+500);
+
