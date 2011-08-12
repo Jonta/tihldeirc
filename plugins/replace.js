70c45e73094b023e652afab95c14ea8315cf5c8d
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-12 11:40:16 +0200

    Replace now support simple *-replace

--- plugins/replace.js
+++ plugins/replace.js
@@ -2,10 +2,33 @@ var bot = require('bot.js'),
 history = require('./history.js'),
 regex = /\bs\/([^\/]+)\/([^\/]+)/;
 
+function calcWord(a, b) {
+    var count = 0,
+    i;
+    for (i = 0; i < a.length; i++) {
+        if (b.indexOf(a[i]) >= 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
 bot.addListener(__filename, 'message', function(from, to, msg) {
+    var last, fixed, word;
     if (msg.match(/^s\//)) {
-        var fixed = history.from[from][1].msg.replace(msg.match(regex)[1], msg.match(regex)[2]);
+        fixed = history.from[from][1].msg.replace(msg.match(regex)[1], msg.match(regex)[2]);
         bot.client.say(to, from + ': ' + fixed);
+    }
+    if (msg.match(/^\w*\*$/)) {
+        word = '';
+        msg = msg.replace(/\*/, '');
+        last = history.from[from][1].msg;
+        last.split(' ').forEach(function(w) {
+            if (calcWord(w, msg) > calcWord(w, word)) {
+                word = w;
+            }
+        });
+        bot.client.say(to, from + ': ' + last.replace(word, msg));
     }
 });
 
