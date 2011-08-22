6177d1edec8714fff8777d03614542f8b0894b54
Eirik Brandtzæg <eirikb@eirikb.no>
2011-08-22 11:53:54 +0200

    Using followed users?

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,18 +1,22 @@
 var bot = require('../bot.js'),
 twitter = require('twitter'),
+winston = require('winston'),
 fs = require('fs'),
 twit = new twitter(JSON.parse(fs.readFileSync('assets/twitter-keys')));
 
-twit.stream('statuses/filter', {
-    track: 'tihlde'
-},
-function(stream) {
+twit.stream('user', function(stream) {
     stream.on('data', function(data) {
-        bot.client.say('#tihlde', '[twitter] ' + data.user.screen_name + ': ' + data.text);
+        winston.info('[twitter]' + JSON.stringify(data));
+        if (data.source && data.target && data.target_object) {
+            bot.client.say('#tihlde', '[twitter] [' + data.source.event + '] ' + data.source.screen_name + ': ' + data.target_object.text);
+        }
     });
 });
 
 bot.onTrigger(__filename, 'Twitter', 'twitter', function(from, to, msg) {
-    twit.updateStatus(from + ': ' + msg, function(data) {});
+    winston.info('[twitter] ' + from + ': ' + to + ' ' + msg);
+    twit.updateStatus(from + ': ' + msg, function(data) {
+        bot.client.say(to, from + ': Jaushø');
+    });
 });
 
