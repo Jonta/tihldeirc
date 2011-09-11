1ada39c0526537912a46fe9d16c2a4d1b418035a
Eirik Brandtzæg <eirikb@eirikb.no>
2011-09-11 20:34:48 +0200

    Blcoked by Googeruu?

--- plugins/calc.js
+++ plugins/calc.js
@@ -2,18 +2,19 @@ var bot = require('../bot.js'),
 get = require('./get.js'),
 winston = require('winston');
 
-bot.addListener(__filename, 'message', function(from, to, msg) {
+bot.onTrigger(__filename, 'Google calc', ['c', 'calc'], function(from, to, msg) {
     get.get({
         host: 'www.google.com',
         path: '/ig/calculator?q=' + encodeURIComponent(msg)
     },
     function(data) {
+        winston.info('[calc] data: ' + data);
         try {
             data = eval('(' + data + ')');
             if (!data.error || data.error.length === 0) {
                 data.rhs = data.rhs.
                 replace(/&#215;/g, '*').
-		replace(/�/g, ' ').
+                replace(/�/g, ' ').
                 replace(/<sup>/g, '^').
                 replace(/<\/sup>/g, '');
                 bot.client.say(to, from + ': ' + data.lhs + ' = ' + data.rhs);
