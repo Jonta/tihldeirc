529362254adf07b75e8ae199645f69cf69ce870f
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-12 15:54:46 +0100

    Awesomeness on the way!

---
+++ hilde.js
@@ -0,0 +1,59 @@
+var fs = require('fs'),
+readline = require('readline');
+
+var ignored, rl = readline.createInterface(process.stdin, process.stdout),
+fname = 'evaled.json';
+
+setTimeout(function() { (function() {
+        ignored = Object.keys(this);
+    })();
+    load();
+},
+1000);
+
+function load() {
+    fs.readFile(fname, function(err, data) {
+        var evaled;
+
+        if (err) console.error(err)
+        else {
+            console.log('data', data.toString());
+            try {
+                evaled = JSON.parse(data);
+                Object.keys(evaled).forEach(function(key) {
+                    eval(key + '=' + evaled[key]);
+                });
+            } catch(e) {
+                console.error(e);
+            }
+        }
+    });
+}
+
+function persist() {
+    var evaled = Object.keys(this).filter(function(key) {
+        return ignored.indexOf(key) < 0;
+    }).reduce(function(o, key) {
+        o[key] = '' + this[key];
+        return o;
+    },
+    {});
+    fs.writeFile(fname, JSON.stringify(evaled));
+}
+
+function onLine(line) {
+    try {
+        var e = eval(line);
+        if (typeof e !== 'undefined') {
+            console.log(e);
+            persist();
+        }
+    } catch(e) {
+        console.log('error', e);
+    }
+}
+
+rl.on('line', function(line) {
+    onLine(line);
+});
+
