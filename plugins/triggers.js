4b52211c25620d9c5dd4c601ea455c4f11b5031b
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-12 04:47:42 +0100

    Removed plugins, like a boss!

--- plugins/triggers.js
+++
@@ -1,8 +0,0 @@
-var bot = require('../bot.js');
-
-bot.onTrigger(__filename, 'Triggerlist', ['help', 'triggers'], function(from, to, msg) {
-    bot.client.say(to, bot.triggers.map(function(t) { 
-        return t.name + ': ' + t.triggerKeys.join(' ');
-    }).join(' - '));
-});
-
