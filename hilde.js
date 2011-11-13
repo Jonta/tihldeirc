06378d1f3ccdc47fd624e3f38e0c0ba7a96be697
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-13 10:53:45 +0100

    Ready for shipping

--- hilde.js
+++ hilde.js
@@ -7,18 +7,19 @@ var readline = require('readline'),
 rl = readline.createInterface(process.stdin, process.stdout);
 
 function traverse(o, cb) {
-        if (Array.isArray(o)) {
-            return o.map(function(e, i) {
-                return traverse(e, cb);
-            });
-        } else if (typeof o === 'object') {
-            return Object.keys(o).reduce(function(o2, key) {
-                o2[key] = traverse(o[key], cb);
-                return o2;
-            }, {});
-        } else {
-            return cb(o);
-        }
+    if (Array.isArray(o)) {
+        return o.map(function(e, i) {
+            return traverse(e, cb);
+        });
+    } else if (typeof o === 'object') {
+        return Object.keys(o).reduce(function(o2, key) {
+            o2[key] = traverse(o[key], cb);
+            return o2;
+        },
+        {});
+    } else {
+        return cb(o);
+    }
 }
 
 function toJSON(o) {
@@ -35,24 +36,20 @@ function fromJSON(json) {
     var data = traverse(JSON.parse(json), function(o) {
         if (typeof o === 'string') {
             try {
-            return eval('temphack2000='+o);
-            } catch (e) {}
+                return eval('temphack2000=' + o);
+            } catch(e) {}
         }
-            return o;
+        return o;
     });
+    delete evaled.temphack2000;
     return data;
 }
 
-var t = toJSON({test: function(){console.log('Real test!')},a:'a',b:1337,z:function(){console.log('LOL!')},
-c: {d: {e: 14.5, f: [1,2, {g: { h: 9, i: function() { return 'lol'}}}]}}});
-console.log(t);
-var f = fromJSON(t);
-console.log(require('util').inspect(f, true, null));
-f.test();
-console.log(f.c.d.f[2].g.i());
-f.z();
-
-
+function keys() {
+    return Object.keys(this).filter(function(key) {
+        return ignored.indexOf(key) < 0;
+    });
+}
 
 rl.on('line', function(line) {
     boss(line, function(e, r) {
@@ -88,7 +85,6 @@ try {
     console.err('Unable to start client', e);
 }
 
-/*
 setTimeout(function() {
     // Refer this to global object
     (function() {
@@ -113,43 +109,32 @@ setTimeout(function() {
             var evaled;
             if (!err) {
                 try {
-                    evaled = JSON.parse(data);
+                    evaled = fromJSON(data);
                     Object.keys(evaled).forEach(function(key) {
-                        try {
-                            eval(key + '=' + evaled[key]);
-                            console.log('- - -', key, '=', evaled[key]);
-                            console.log(typeof this[key]);
-                            console.log(Array.isArray(this[key]));
-                        } catch(e) {
-                            console.log(e);
-                            this[key] = evaled[key];
-                        }
+                        this[key] = evaled[key];
                     });
                 } catch(e) {}
             }
+            /*
             if (Array.isArray(this.listeners)) {
-            this.listeners.forEach(function(listener) {
-                console.log('ADD LISTENER', listener);
-                //client.on('message', listener);
-            });
+                this.listeners.forEach(function(listener) {
+                    console.log('ADD LISTENER', listener);
+                    //client.on('message', listener);
+                });
             }
-            persist();
+            */
         });
     } ())
 },
 1)
-*/
 
 function persist() {
-            var evaled = Object.keys(this).filter(function(key) {
-                return ignored.indexOf(key) < 0;
-            }).reduce(function(o, key) {
-                o[key] = hackify(this[key]);
-                return o;
-            },
-            {});
-
-    fs.writeFile(fname, JSON.stringify(evaled));
+    var evaled = keys().reduce(function(o, key) {
+        o[key] = this[key];
+        return o;
+    },
+    {});
+    fs.writeFile(fname, toJSON(evaled));
 }
 
 function boss(line, cb) {
